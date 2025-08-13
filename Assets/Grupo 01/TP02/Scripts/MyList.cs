b3969ab99916c85f8843b4dc7ddba2b5f5f1389b
laureanoan0 <laureanomeyer22@gmail.com>
2025-08-12 22:50:43 -0300

    Add - TP02 Basics
    Created MyNode
    Created MyNode
    Created TP02Executer

---
+++ Assets/Grupo 01/TP02/Scripts/MyList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLinkedList;
+using NUnit.Framework;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+
+
+public class MyList <T>
+ {
+    private MyNode<T> root;
+    private MyNode<T> tail;
+    private List<T> list;
+    private bool hasRoot = false;
+
+    public int Count { get; private set; }
+    public T this[int index]
+    {
+        get { return list[index]; }
+        set
+        {
+            if (index < list.Count)
+                list[index] = value;
+        }
+    }
+
+
+    public MyList(T rootValue)
+    {
+        list = new List<T>();
+        root = new MyNode<T>(rootValue, -1, 1, 0);
+    }
+
+    public void Add(T value) { }
+
+    public void AddRange(T[] values) { }
+
+    public void AddRange(MyList<T> values) { }
+
+    public void Remove(T value) { }
+
+    public void RemoveAt(int index) { }
+
+    public void Insert(int index, T value) { }
+
+    //public bool IsEmpty() {  }
+
+    public void Clear() { }
+
+    //public override string ToString()
+    //{
+    //    string text = "";
+
+    //    for (int i = 0; i < counter; i++)
+    //        text += arrayD[i].ToString() + ", ";
+
+    //    return text;
+
+    //}
+
+
+
+}
+
---
+++ Assets/Grupo 01/TP02/Scripts/MyNode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor.Experimental.GraphView;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+
+namespace MyLinkedList
+{
+    public class MyNode<T>
+    {
+
+        private T value;
+        private int nextIndex, prevIndex, index;
+
+        public MyNode(T value,int prevIndex, int nextIndex, int index)
+        {
+            this.value = value;
+            this.index = index; 
+            this.nextIndex = nextIndex;
+            this.prevIndex = prevIndex;
+        }
+
+        public bool isEquals(T value)
+        {
+
+            if (this.value.Equals(value)) return true;
+            else return false;
+
+        }
+
+        public override string ToString()
+        {
+            string text = "";
+
+            text += value.ToString();
+
+            return text;
+
+        }
+    }
+}
