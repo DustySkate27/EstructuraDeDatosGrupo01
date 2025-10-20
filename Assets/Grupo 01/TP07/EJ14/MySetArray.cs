51f1ab9672b72e1a942a868c21fdaa66b391813c
laureanoan0 <laureanomeyer22@gmail.com>
2025-10-20 18:58:19 -0300

    Added - MySetArray, fully functional

--- Assets/Grupo 01/TP07/EJ14/MySetArray.cs
+++ Assets/Grupo 01/TP07/EJ14/MySetArray.cs
@@ -2,44 +2,44 @@
 using UnityEngine;
 
 
-public class MySetList<T> : MySet<T>
+public class MySetArray<T> : MySet<T>
 {
     private MyList<T> set;
 
-    public MySetList(T item)
+    public MySetArray(T item)
     {
         set = new MyList<T>();
         set.Add(item);
     }
-    public MySetList()
+    public MySetArray()
     {
         set = new MyList<T>();
     }
 
-    public override void Add(T item) 
-    { 
+    public override void Add(T item)
+    {
         if (!set.Contains(item)) set.Add(item);
     }
-    public override void Remove(T item) 
+    public override void Remove(T item)
     {
         set.Remove(item);
     }
-    public override void Clear() 
+    public override void Clear()
     {
         set.Clear();
     }
-    public override bool Contains(T item) 
-    { 
+    public override bool Contains(T item)
+    {
         return set.Contains(item);
     }
-    public override void Show() 
+    public override void Show()
     {
-        for (int i = 0; i < set.Counter; i++) 
+        for (int i = 0; i < set.Counter; i++)
         {
             Debug.Log(set[i]);
         }
     }
-    public override string _ToString() 
+    public override string _ToString()
     {
         string stringToShow = "";
 
@@ -50,12 +50,12 @@ public class MySetList<T> : MySet<T>
 
         return stringToShow;
     }
-    public override int Cardinality() 
-    { 
+    public override int Cardinality()
+    {
         return set.Counter;
     }
-    public override bool IsEmpty() 
-    { 
+    public override bool IsEmpty()
+    {
         return set.IsEmpty();
     }
     public override MySet<T> Union(MySet<T> other)
@@ -63,20 +63,20 @@ public class MySetList<T> : MySet<T>
         SimpleList<T> otherArray = new SimpleList<T>();
         MySetList<T> result = new MySetList<T>();
 
-        for (int i = 0; i < other.Cardinality(); i++) 
+        for (int i = 0; i < other.Cardinality(); i++)
         {
-            if (!set.Contains(other.Elements()[i])) 
-            { 
+            if (!set.Contains(other.Elements()[i]))
+            {
                 otherArray.Add(other.Elements()[i]);
             }
-        } 
+        }
 
         for (int i = 0; i < set.Counter; i++) result.Add(set[i]);
         for (int i = 0; i < otherArray.Count; i++) result.Add(otherArray[i]);
 
         return result;
     }
-    public override MySet<T> Intersect(MySet<T> other) 
+    public override MySet<T> Intersect(MySet<T> other)
     {
         SimpleList<T> otherArray = new SimpleList<T>();
         MySetList<T> result = new MySetList<T>();
@@ -88,12 +88,12 @@ public class MySetList<T> : MySet<T>
                 otherArray.Add(other.Elements()[i]);
             }
         }
-     
+
         for (int i = 0; i < otherArray.Count; i++) result.Add(otherArray[i]);
 
         return result;
     }
-    public override MySet<T> Difference(MySet<T> other) 
+    public override MySet<T> Difference(MySet<T> other)
     {
         MySetList<T> result = new MySetList<T>();
 
@@ -109,8 +109,9 @@ public class MySetList<T> : MySet<T>
     {
         T[] arrayToReturn = new T[set.Counter];
 
-        for(int i = 0; i < set.Counter; i++) arrayToReturn[i] = set[i];
+        for (int i = 0; i < set.Counter; i++) arrayToReturn[i] = set[i];
 
         return arrayToReturn;
     }
 }
+
